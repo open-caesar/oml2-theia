e661b1611fb37172b3c0e3e6e49bb603651033e7
Maged Elaasar <elaasar@jpl.nasa.gov>
2018-12-05 15:16:16 -0800

    Renamed the apps and switched to non-docker CI

--- open.caesar.oml2.theia.parent/oml2/src/frontend/language/oml2-commands.ts
+++ open.caesar.oml2.theia.parent/oml2/src/frontend/language/oml2-commands.ts
@@ -1,10 +1,3 @@
-/*
- * Copyright (C) 2017 TypeFox and others.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- */
-
 import { inject, injectable } from "inversify";
 import { CommandContribution, CommandRegistry, Command } from '@theia/core/lib/common';
 import { EditorCommands } from "@theia/editor/lib/browser";
